2a28c17162f6505b2647afc6ccde8a7a13f8034a
AntonGrn <45007883+AntonGrn@users.noreply.github.com>
2021-02-20 17:54:50 +0100

    login & register dropdown

--- public/script/script.js
+++ public/script/script.js
@@ -1,16 +1,18 @@
 $(document).ready(function () {
-    screenConfig.onResize();       
+    $("#main-wrapper").addClass("not-logged-in"); // Remove for dev purposes
+    sidebar.onResize();       
 });
 
 $(window).resize(function () {
-    screenConfig.onResize();
+    sidebar.onResize();
 });
+
 
 // ___________________________ SCREEN SIZE, MENUES & BEHAVIOUR _________________________________
 
 /* Object literal: Avoid collisions/overwrites with global window properties/variables. */
 /* A similar approach would be the Module Pattern, using an anonymous function instead of an object. */
-const screenConfig = {
+const sidebar = {
     minWidthContent: 440, // Used for dynamic sidebar handling. Set to smallest width of content display.
     minWidthSidebar: 300,
     menuDisplayed: false,
@@ -41,9 +43,95 @@ const screenConfig = {
             this.toggleMenu();
         }
         $("#"+item).toggleClass("dropdown");
-    },
-    clientAdapt: function(clientProfile) { // defualt(not looged in) / developer / admin
-        // Adapt view to type of client
-
     }
 };
+
+const signIn = {  // Drop down from header
+    approach: "login", // 'login' / 'register'
+    signInDisplayed: false,
+    toggleSignIn: function(approach) {
+        if(this.signInDisplayed && this.approach == approach) {
+            this.hideSignIn();
+        } else {
+            /* Instead of toggle slide down: Switch between Register/Login */
+            $("#input-mail").val("");
+            $("#input-pwd").val("");
+            this.showSignIn(approach);
+        }
+    },
+    showSignIn: function(approach) {
+        $("#content").fadeTo("slow", 0.4); // Fade background
+        $("#sign-in-label").text(approach=="login" ? "Enter login credentials" : "Create new account");
+        $("#sign-in").addClass("display"); // Slide in div
+        this.approach = approach;
+        this.signInDisplayed = true;
+        if(approach == "register") {
+            $("#link-demo").addClass("hide");
+        } else {
+            $("#link-demo").removeClass("hide");
+        }
+    },
+    hideSignIn: function() { 
+        $("#sign-in").removeClass("display");
+        $("#content").fadeTo("slow", 1); // Unfade background
+        this.signInDisplayed = false;
+    },
+    submitSignIn: function() {
+        if(this.approach == "login") {
+            
+            alert("Login");
+            
+        } else {
+            alert("Regsiter")
+        }
+        this.hideSignIn();
+    }
+};
+
+const clientManager = {
+    client: null,
+    setClient: function(client) {
+        this.client = client;
+    },
+    viewAdapt: function(clientProfile) { 
+        // Adapt view to type of client
+        $("#main-wrapper").removeClass("not-logged-in");
+        $("#main-wrapper").removeClass("developer");
+        $("#main-wrapper").removeClass("admin");
+        $("#main-wrapper").addClass(clientProfile);
+        if(clientProfile == "developer") {
+            $("#label-name").text(this.client.name.toUpperCase());
+        } else if(clientProfile == "admin") {
+            $("#label-name").text("ADMIN");
+        }
+    },
+    logout: function() {
+        if(demoMode.isOn) {
+            demoMode.end();
+        };
+        this.viewAdapt("not-logged-in");
+    }
+};
+
+const demoMode = {
+    isOn: false,
+    demoClient: 
+    {
+        email: "john@mail.com",
+        name: "John Ohlsson",
+        isAdmin: false
+    },
+    start: function() { // 'developer' / 'admin'
+        clientManager.setClient(this.demoClient);
+        clientManager.viewAdapt("developer");
+        $("#main-wrapper").addClass("demo-mode");
+        this.isOn = true;
+        signIn.hideSignIn();
+        refreshContent();
+    },
+    end: function() {
+        this.isOn = false;
+        $("#main-wrapper").removeClass("demo-mode");
+
+    }
+}
