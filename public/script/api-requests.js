6656e6804ff56cfc278079a81de8aaa5f6bce171
AntonGrn <45007883+AntonGrn@users.noreply.github.com>
2021-02-22 09:20:36 +0100

    edit profile

---
+++ public/script/api-requests.js
@@ -0,0 +1,15 @@
+const apiRequest = {
+    login: function(mail, pwd) {
+        // Implement
+    },
+    logout: function() {
+        // Implement
+    },
+    getDevelopers: function(filter) {
+        // Implement
+    },
+    updateProfile: function(updatedClient) {
+        alert(updatedClient.name);
+        // Send update request to server
+    }
+}
--- public/script/script.js
+++ public/script/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     $("#main-wrapper").addClass("not-logged-in"); // Remove for dev purposes
-    sidebar.onResize();       
+    sidebar.onResize();   
+    
+    demoMode.start(); // TEMP FOR DEV
 });
 
 $(window).resize(function () {
@@ -43,6 +45,98 @@ const sidebar = {
             this.toggleMenu();
         }
         $("#"+item).toggleClass("dropdown");
+    },
+    setProfileSection: function() {
+        const client = clientManager.client;
+        if(!client.isAdmin) {
+            const $profileSection = $("#edit-profile");
+            $profileSection.find("#name").val(client.name);
+            $profileSection.find("#mail").val(client.mail);
+            $profileSection.find("#profession").val(client.professionLabel);
+            $profileSection.find("#age").val(client.age);
+            $profileSection.find("#country").val(client.country);
+            $profileSection.find("#years-experience").val(client.yearsExperience);
+            $profileSection.find("#hour-rate").val(client.pricePerHour);
+            $profileSection.find("#description").val(client.selfDescription);
+            
+            // Load all available skillsets from server -> MAYBE GET AT LOGIN???
+            const availableSkills = skillsets;
+            // Convert skillsets array to map for key-value access (skill, rating)
+            let skillMap = new Map();
+            for (availableSkill of availableSkills) {
+                skillMap.set(availableSkill, 0); //set skill rate to default 0
+            };
+            // Integrate client's skillset ratings
+            for(clientSkill of client.skillset) {
+                skillMap.set(clientSkill.skill, clientSkill.rate);
+            };
+            // Sort skills on ratings
+
+            // Update Profile section in sidebar
+            const $skillSection = $profileSection.find("#skillset");
+            // Clear div
+            $skillSection.empty();
+            // Rebuild div (add sliders for each skill)
+            for(let [key, value] of skillMap) {
+                var skill = key;
+                var rating = value;
+                $skillSection.append(
+            `<label for="${skill}">${skill}</label>
+            <input type="range" id="${skill}" class="slider" min="0" max="10" value="${rating}"><br>`);
+            };
+            // Add evenlisteners for custom slider behaviours
+            var sliders = document.querySelectorAll("#skillset .slider");
+            
+            sliders.forEach(function(slider) {
+                updateSlider(slider);       
+                slider.addEventListener("mousemove", function() {updateSlider(slider)});
+                slider.addEventListener("click", function() {updateSlider(slider)});
+            });
+
+            // Event handler for skillset sliders
+            function updateSlider (slider) {
+                var x = slider.value * 10;
+                    var color = "linear-gradient(90deg, var(--theme-main) " +  x + "%, grey " + x + "%)";
+                    slider.style.background = color;
+            };
+            // Event handler for submit profile changes
+            $("#profile-form").on("submit", function(event) {
+                event.preventDefault(); // Prevent redirect or reload, but still validate form fields.
+                sidebar.submitProfileUpdate();
+            });
+        };
+    },
+    submitProfileUpdate: function() {
+        // See eventlistener setup in 'this.setProfileSection()'
+        // Build an updated client object:
+        const upd_client = clientManager.client;
+        // 1. Collect updatable fields from profile section:
+        const $profileSection = $("#edit-profile");
+        upd_client.name = $profileSection.find("#name").val();
+        upd_client.mail = $profileSection.find("#mail").val();
+        upd_client.professionLabel = $profileSection.find("#profession").val();
+        upd_client.age = $profileSection.find("#age").val();
+        upd_client.country = $profileSection.find("#country").val();
+        upd_client.yearsExperience = $profileSection.find("#years-experience").val();
+        upd_client.pricePerHour = $profileSection.find("#hour-rate").val();
+        upd_client.description = $profileSection.find("#description").val();
+        // 2. Collect and filter skills with rating > 0
+        const upd_skillset = [];
+        const skillSliders = document.querySelectorAll("#skillset .slider");
+        for(skillSlider of skillSliders) {
+            skillName = skillSlider.id;
+            skillRate = skillSlider.value;
+            if(skillRate > 0) {
+                upd_skillset.push({
+                    skill: skillName,
+                    rate: skillRate
+                });
+            };
+        };
+        // Overwrite current skillset
+        upd_client.skillset = upd_skillset;
+        // 3. Make server request
+        apiRequest.updateProfile(upd_client); 
     }
 };
 
@@ -92,6 +186,7 @@ const clientManager = {
     client: null,
     setClient: function(client) {
         this.client = client;
+        sidebar.setProfileSection();
     },
     viewAdapt: function(clientProfile) { 
         // Adapt view to type of client
@@ -133,15 +228,48 @@ const popup = {
         $("#content-wrapper").fadeTo("slow", 1); // Unfade background
 
     }
-}
+};
 
 const demoMode = {
     isOn: false,
     demoClient: 
     {
-        email: "john@mail.com",
+        _id: 1,
+        mail: "john@mail.com",
+        isAdmin: false,
         name: "John Ohlsson",
-        isAdmin: false
+        professionLabel: "Web developer",
+        age: 30,
+        country: "SWEDEN",
+        yearsExperience: 5,
+        pricePerHour: 1100,
+        rating: 4.5,
+        ratings: 7,
+        memberSince: "2020-09-12",
+        selfDescription: "A dedicated software developer.",
+        banUntil: "never",
+        skillset: [
+            {
+                skill: "Java",
+                rate: 8
+            },
+            {
+                skill: "HTML",
+                rate: 2
+            },
+            {
+                skill: "JavaScript",
+                rate: 4
+            },
+            {
+                skill: "Python",
+                rate: 3
+            },
+            {
+                skill: "MySql",
+                rate: 6
+            }  
+        ]  
     },
     start: function() { // 'developer' / 'admin'
         clientManager.setClient(this.demoClient);
@@ -160,4 +288,4 @@ const demoMode = {
         popup.display("Demo mode ended", ["Hope you liked it."]);
 
     }
-}
+};
